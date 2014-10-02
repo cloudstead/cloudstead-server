eb6fc86be9397c80876d3e90605afa9124503ad2
jonathan <jonathan@kyuss.org>
2014-10-02 12:32:13 -0700

    fix path

--- gen-sql.sh
+++ gen-sql.sh
@@ -3,6 +3,6 @@
 BASE=$(cd $(dirname $0) && pwd)
 cd ${BASE}
 
-outfile=${BASE}/../cloudos-apps/apps/cloudstead/files/cloudstead.sql
+outfile=${BASE}/../cloudstead-apps/apps/cloudstead/files/cloudstead.sql
 
-${BASE}/../cloudos-lib/gen-sql.sh cloudstead_test ${outfile}
+${BASE}/../cloudos/cloudos-lib/gen-sql.sh cloudstead_test ${outfile}
