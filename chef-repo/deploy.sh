1a7e609ffdbafb1ee56453ea0cc20490f0d590e4
jonathan <jonathan@kyuss.org>
2014-10-03 09:53:41 -0700

    deploy appstore-server alongside cloudstead server (for now)

--- chef-repo/deploy.sh
+++ chef-repo/deploy.sh
@@ -41,6 +41,8 @@ data_bags/cloudstead/init.json \
 data_bags/cloudstead/ports.json \
 data_bags/cloudos-dns/init.json \
 data_bags/cloudos-dns/ports.json \
+data_bags/cloudos-appstore/init.json \
+data_bags/cloudos-appstore/ports.json \
 certs/cloudstead/ssl-https.key \
 certs/cloudstead/ssl-https.pem \
 certs/cloudstead/ssl-https-wildcard.key \
