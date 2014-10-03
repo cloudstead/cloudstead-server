86e554f6360fb7872d466feb6e20c929fcdae874
jonathan <jonathan@kyuss.org>
2014-10-03 08:06:35 -0700

    fix sql generation

--- src/test/java/cloudos/cloudstead/DbInit.java
+++ src/test/java/cloudos/cloudstead/DbInit.java
@@ -1,8 +1,11 @@
 package cloudos.cloudstead;
 
+import cloudos.cloudstead.resources.ApiResourceITBase;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-public class DbInit {
+@Slf4j
+public class DbInit extends ApiResourceITBase {
 
     // noop for now
     @Test public void initDatabase () throws Exception {}
