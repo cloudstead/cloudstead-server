570d049a3dbe5e6985a6af7c9a41d19715567858
jonathan <jonathan@kyuss.org>
2015-01-23 18:10:28 -0800

    add column sizes for ServiceKey entity, ensure data can fit

--- src/main/java/cloudos/cloudstead/model/ServiceKey.java
+++ src/main/java/cloudos/cloudstead/model/ServiceKey.java
@@ -4,13 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.cobbzilla.wizard.model.IdentifiableBase;
+import org.cobbzilla.wizard.validation.HasValue;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Size;
 
 @Entity @Accessors(chain=true)
 public class ServiceKey extends IdentifiableBase {
 
+    @HasValue(message="err.host.empty")
+    @Column(length=1024, nullable=false, updatable=false)
+    @Size(max=1024)
     @Getter @Setter private String host;
+
+    @HasValue(message="err.key.empty")
+    @Column(length=4096, unique=true, nullable=false, updatable=false)
+    @Size(max=4096)
     @Getter @Setter private String key;
 
 }
