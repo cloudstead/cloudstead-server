2efbea5b99a2202b859113594f570c4e2bd6a1d8
jonathan <jonathan@kyuss.org>
2015-02-07 03:40:04 -0800

    fix args handling

--- src/main/java/cloudos/cloudstead/main/ActivationCodeMain.java
+++ src/main/java/cloudos/cloudstead/main/ActivationCodeMain.java
@@ -8,7 +8,7 @@ public class ActivationCodeMain {
 
     public static void main (String[] args) {
         final String redisKey = CommandShell.execScript("cd ~cloudstead && cat .cloudstead.env | grep CLOUD_STORAGE_DATA_KEY | tr '=' ' ' | awk '{print $3}'").trim();
-        ArrayUtil.append(args, redisKey);
+        args = ArrayUtil.append(args, redisKey);
         ActivationCodeService.main(args);
     }
 
