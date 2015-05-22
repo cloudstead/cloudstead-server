a4d3b429139f03b5948211d2ac73a72f2a86c533
jonathan <jonathan@kyuss.org>
2015-05-22 15:10:30 -0700

    fix missing semicolon

--- csapp/app/js/mixins/signup_mixin.js
+++ csapp/app/js/mixins/signup_mixin.js
@@ -96,7 +96,7 @@ App.SignupMixin = Ember.Mixin.create({
 
 	_handleLoginCredentialError: function(validationErrors) {
 		// TODO implement this if needed.
-		console.log("LoginCredentialError => ", validationErrors)
+		console.log("LoginCredentialError => ", validationErrors);
 	},
 
 	_handleSuccessfulLogin: function(){
