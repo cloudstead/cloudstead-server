6c83c3e26fb4c5a26c0e88e3aa5938c411ef6892
jonathan <jonathan@kyuss.org>
2015-06-02 22:33:01 -0700

    update edition map for rackspace

--- src/main/java/cloudos/cloudstead/model/support/CloudOsEdition.java
+++ src/main/java/cloudos/cloudstead/model/support/CloudOsEdition.java
@@ -14,42 +14,45 @@ import static cloudos.cslib.compute.meta.CsCloudTypeFactory.*;
 import static org.cobbzilla.util.daemon.ZillaRuntime.die;
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 
+/**
+ * This is where we maintain the mapping of edition->hardware profile, for CsCloudTypes supporting that edition.
+ */
 @AllArgsConstructor
 public enum CloudOsEdition {
 
     starter   ((Map) ImmutableMap.of(
-                    DO, DO.getInstanceType("2gb"),
-                    RS, RS.getInstanceType("placeholder"))),
+                    DO, DO.type("2gb"),
+                    RS, RS.type("general1-1"))),
 
     master    ((Map) ImmutableMap.of(
-                    EC2, EC2.getInstanceType("m3.medium"),
-                    DO, DO.getInstanceType("4gb"),
-                    RS, RS.getInstanceType("placeholder"))),
+                    EC2, EC2.type("m3.medium"),
+                    DO, DO.type("4gb"),
+                    RS, RS.type("general1-2"))),
 
     business   ((Map) ImmutableMap.of(
-                    EC2, EC2.getInstanceType("r3.large"),
-                    DO, DO.getInstanceType("8gb"),
-                    RS, RS.getInstanceType("placeholder"))),
+                    EC2, EC2.type("r3.large"),
+                    DO, DO.type("8gb"),
+                    RS, RS.type("compute1-4"))),
 
     king       ((Map) ImmutableMap.of(
-                    EC2, EC2.getInstanceType("r3.xlarge"),
-                    DO, DO.getInstanceType("16gb"),
-                    RS, RS.getInstanceType("placeholder"))),
+                    EC2, EC2.type("r3.xlarge"),
+                    DO, DO.type("16gb"),
+                    RS, RS.type("compute1-8"))),
 
     emperor    ((Map) ImmutableMap.of(
-                    EC2, EC2.getInstanceType("r3.2xlarge"),
-                    DO, DO.getInstanceType("32gb"),
-                    RS, RS.getInstanceType("placeholder"))),
+                    EC2, EC2.type("r3.2xlarge"),
+                    DO, DO.type("32gb"),
+                    RS, RS.type("compute1-15"))),
 
     maximus    ((Map) ImmutableMap.of(
-                    EC2, EC2.getInstanceType("r3.4xlarge"),
-                    DO, DO.getInstanceType("48gb"),
-                    RS, RS.getInstanceType("placeholder"))),
+                    EC2, EC2.type("r3.4xlarge"),
+                    DO, DO.type("48gb"),
+                    RS, RS.type("compute1-30"))),
 
     supermax   ((Map) ImmutableMap.of(
-                    EC2, EC2.getInstanceType("r3.8xlarge"),
-                    DO, DO.getInstanceType("64gb"),
-                    RS, RS.getInstanceType("placeholder")));
+                    EC2, EC2.type("r3.8xlarge"),
+                    DO, DO.type("64gb"),
+                    RS, RS.type("compute1-60")));
 
     @Getter private final Map<CsCloudType<? extends CsCloud>, CsInstanceType> instanceTypes;
 
