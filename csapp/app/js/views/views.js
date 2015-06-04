867114860ca03c0020dc508674301c7e5accdcc9
Bojan Mojsilovic <bmojsilovic@itekako.com>
2015-06-04 15:22:08 +0200

    Signup form need to have empty input fields

--- csapp/app/js/views/views.js
+++ csapp/app/js/views/views.js
@@ -7,7 +7,19 @@ App.ApplicationView = Ember.View.extend({
 App.IndexView = Ember.View.extend({
     initFoundation: function () {
         Ember.$(document).foundation();
-    }.on('didInsertElement')
+    }.on('didInsertElement'),
+	initEvents: function(){
+		// ON SIGN UP, ALL INPUT FIELDS NEED TO BE EMPTY
+		$(document).on('opened.fndtn.reveal', '#signupModal', function () {
+			var modal = $(this);
+			$.each(modal.find("input"), function(index, item){
+				if($(item).attr('type') === "text" || $(item).attr('type') === "password"){
+					$(item).val("");
+				}
+
+			});
+		});
+	}.on('didInsertElement')
 });
 
 App.AdminDetailsView = Ember.View.extend({
