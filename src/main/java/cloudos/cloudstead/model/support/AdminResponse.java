ee550b95b1772f406b226eb499df1dd706b45101
jonathan <jonathan@kyuss.org>
2015-06-05 14:50:41 -0700

    simplify

--- src/main/java/cloudos/cloudstead/model/support/AdminResponse.java
+++ src/main/java/cloudos/cloudstead/model/support/AdminResponse.java
@@ -6,13 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.cobbzilla.util.reflect.ReflectionUtil;
 
+import static org.cobbzilla.util.reflect.ReflectionUtil.copy;
+
 @NoArgsConstructor
 public class AdminResponse extends Admin {
 
     @Getter @Setter private String session;
 
     public AdminResponse(Admin admin, String sessionId) {
-        ReflectionUtil.copy(this, admin);
+        copy(this, admin);
         setSession(sessionId);
     }
 
